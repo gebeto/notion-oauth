f6ec14b4cc9f7fb516fccc20cf0b81e8c18fb517
gebeto <slavik.nychkalo@gmail.com>
2022-04-29 22:41:36 +0300

    add notion cors proxy

---
+++ api/api.js
@@ -0,0 +1,14 @@
+const fetch = require("@vercel/fetch")(require("node-fetch"));
+
+module.exports = async (req, res) => {
+  const result = await fetch(`https://api.notion.com${req.query.path}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "notion-version": req.headers["notion-version"],
+      Authorization: req.headers["authorization"],
+    },
+    body: JSON.stringify({}),
+  });
+  result.body.pipe(res);
+};
---
+++ api/test.js
@@ -0,0 +1,6 @@
+const fetch = require("@vercel/fetch")(require("node-fetch"));
+
+module.exports = async (req, res) => {
+  const result = await fetch(`https://httpbin.org/get`);
+  result.body.pipe(res);
+};
